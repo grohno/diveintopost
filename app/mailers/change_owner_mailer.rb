14d431685a0ae5fa02403991ab6ee063b4c31110
yuki ohno <jeanadler33@gmail.com>
2021-05-20 06:19:17 +0000

    RuboCopを使ってコード解析＆修正

--- app/mailers/change_owner_mailer.rb
+++ app/mailers/change_owner_mailer.rb
@@ -2,5 +2,5 @@ class ChangeOwnerMailer < ApplicationMailer
   def change_owner_mail(change_owner)
     @change_owner = change_owner
     mail to: @change_owner.email, subject: I18n.t('views.messages.change_owner_mail_subject')
-    end
+  end
 end
